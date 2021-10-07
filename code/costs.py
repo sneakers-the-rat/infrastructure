fa1902553d054d6f668c2f8b0837c2afece905b5
sneakers-the-rat <JLSaunders987@gmail.com>
2021-10-07 16:02:34 -0700

    more on health surveillance

--- code/costs.py
+++ code/costs.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-bdt = pd.read_csv('../_data/REPORTER_bdt_04Oct2021_111729.csv', skiprows=4)
-strides = pd.read_csv('../_data/REPORTER_STRIDES_05Oct2021_084456.csv', skiprows=5)
+bdt = pd.read_csv('../data/REPORTER_bdt2_07Oct2021_043559.csv', skiprows=8)
+strides = pd.read_csv('../data/REPORTER_STRIDES_05Oct2021_084456.csv', skiprows=5)
+commons = pd.read_csv('../data/REPORTER_commons_07Oct2021_054910.csv', skiprows=4)
 
 print(f'Total Cost of Biomedical Data Translator {bdt["Total Cost IC"].sum()}')
 print(f'Total Cost of STRIDES: {strides["Total Cost IC"].sum()}')
+print(f'Total Cost of Commons: {commons["Total Cost IC"].sum()}')
